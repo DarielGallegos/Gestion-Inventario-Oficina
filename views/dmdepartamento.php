a4c3370505df9823c18756dd89d2084cffe3849b
draymond22 <118583267+draymond22@users.noreply.github.com>
2024-03-11 07:50:41 -0600

    Data Maestro Departamento
    formulario Datos Maestro Departamento

---
+++ views/dmdepartamento.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro de Departamentos</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+
+<h1>Registro de Departamentos</h1>
+<form method="post" action="departamentos.php">
+    ID: <input type="text" name="id" readonly>
+    Nombre: <input type="text" name="nombre" required><br><br>
+ <input type="submit" value="Agregar">
+</form>
+
+<table>
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+        </tr>
+    </thead>
+    <tbody>
+  
+    </tbody>
+</table>
+
+</body>
+</html>
+
