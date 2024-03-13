3ace8065f9f3fa586569c3b20da5a3510cba5cc4
draymond22 <118583267+draymond22@users.noreply.github.com>
2024-03-12 21:51:14 -0600

    Formularios Data MAestro departamento
    Formularios Data MAestro departamento

--- views/dmdepartamento.php
+++ views/dmdepartamento.php
@@ -1,32 +1,61 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registro de Departamentos</title>
-    <link rel="stylesheet" type="text/css" href="styles.css">
+    <title>Gestion Inventario Oficina</title>
+    <link rel="stylesheet" href=".././css/bootstrap.min.css">
+    <link rel="stylesheet" href=".././css/index.css">
+    <link rel="stylesheet" href=".././css/globalStyle.css">
+
+    
 </head>
+
 <body>
+    <?php include('.././components/nav-bar.php'); ?>
 
-<h1>Registro de Departamentos</h1>
-<form method="post" action="departamentos.php">
-    ID: <input type="text" name="id" readonly>
-    Nombre: <input type="text" name="nombre" required><br><br>
- <input type="submit" value="Agregar">
-</form>
+    <section class="mt-container container-fluid"><br><br>
+        <h2 class="text-center">Registro de Departamentos</h2><br><br>
+        <section class="container-fluid row gx-5 px-4">
 
-<table>
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>Nombre</th>
-        </tr>
-    </thead>
-    <tbody>
-  
-    </tbody>
-</table>
+            <!-- Inicio Estructura de Formulario Registro -->
+            <section class=" col col-6 col-md-4">
+                <p class="text-center paragraph">Formulario de Registro</p>
+                <form>
+                    <label for="idDepartamento" class="form-label" >ID Departamento: </label>
+                    <input type="text" class="form-control" id="idDepartamento" name="idDepartamento" readonly>
+                    <br>
+                    <label for="NoDepartamento" class="form-label">Nombre del Departamento</label>
+                    <input type="text" class="form-control" id="NoDepartamento" name="NoDepartamento"  required>
+                    <br>
+                </form>
+                <section class="btn-group mt-4">
+                    <button type="button" class="btn btn-outline-primary" id="btnAgregar">Agregar</button>
+                </section>
+            </section>
+            <!-- Fin Estructura de Formulario Registro -->
 
+
+            <!-- Inicio Estructura de Tabla -->
+            <section class="col">
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>ID PRODUCTO</th>
+                            <th>Nombre Producto</th>
+                            <th>Cantidad</th>
+                        </tr>
+                    </thead>
+                    <tbody id="contentTable">
+                    </tbody>
+                </table>
+            </section>
+            <!-- Fin Estructura de Tabla -->
+        </section>
+    </section>
 </body>
-</html>
+<script src=".././js/bootstrap.bundle.min.js"></script>
 
+</html>
