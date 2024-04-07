6277b8600b0b5f919469f17d801639f5f7ce4eab
DempseyPrimi <72927786+DempseyPrimi@users.noreply.github.com>
2024-04-07 17:13:05 -0600

    Add Generator Report
    Se creo la utilidad de generar reporte, y se le un estilo general a los reportes.

--- js/consultaReporteria.js
+++ js/consultaReporteria.js
@@ -1,33 +1,117 @@
-function mostrar() {
+window.onload = () =>{
+    
+}
 
+function mostrar(e) {
+    if (!toggle_table_grafico) {
+        document.getElementById('tabla').style.display = 'block';
+        document.getElementById('grafico').style.display = 'none';
+        toggle_table_grafico = true;
+        return;
+    }
+
+    document.getElementById('tabla').style.display = 'none';
+    document.getElementById('grafico').style.display = 'block';
+    toggle_table_grafico = false;
+
+}
+function mostrar() {
     document.getElementById('tablaE').style.display = 'block';
     document.getElementById('grafico').style.display = 'none';
+}
+let toggle_table_grafico = false;
+function mostrar(e) {
+    if (!toggle_table_grafico) {
+        document.getElementById('tabla').style.display = 'block';
+        document.getElementById('grafico').style.display = 'none';
+        toggle_table_grafico = true;
+        return;
+    }
+
+    document.getElementById('tabla').style.display = 'none';
+    document.getElementById('grafico').style.display = 'block';
+    toggle_table_grafico = false;
 
 }
 
-google.charts.load('current', {
-    'packages': ['corechart']
-});
-google.charts.setOnLoadCallback(dibujarGrafico);
+function generarReporte(pet){
+    const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 4000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.onmouseenter = Swal.stopTimer;
+            toast.onmouseleave = Swal.resumeTimer;
+        }
+    });
+    Swal.fire({
+        title: 'Formulario Creacion de Reporte',
+        html: ` <form>
+                    <label for="fileName">Encabezado del Reporte:  
+                    <input type="text" class="form-control" id="encabezadoReporte">
+                    <label for="fileName">Nombre del Archivo de Reporte 
+                    <input type="text" class="form-control" id="nameFile">
+                    <label form="descripcionReport">Descripcion del Reporte
+                    <textarea id="descripcionReport" class="form-control" cols="30" rows="5"></textarea>
+                    <label for="fechaGeneracion">Fecha de Generacion de Reporte:
+                    <input type="date" id="dateCreate" class="form-control" disabled> 
+                    <label for="fechaGeneracion">Fecha de Filtro Para Reporte de Pedido (Solamente):
+                    <input type="date" id="dateFilter" class="form-control"> 
+                </form>
+                `,
+        showCancelButton: true,
+        confirmButtonText: 'Generar',
+        cancelButtonText: 'Cancelar',
+        didOpen: () => {
+            var today = new Date();
+            if(today.getDate > 10){
+                document.getElementById("dateCreate").value = today.getFullYear() +'-'+('0'+(today.getMonth()+1))+'-'+ (today.getDate());
+                document.getElementById("dateFilter").value = today.getFullYear() +'-'+('0'+(today.getMonth()+1))+'-'+ (today.getDate());
+            }else{
+            document.getElementById("dateCreate").value = today.getFullYear() +'-'+('0'+(today.getMonth()+1))+'-'+ ('0'+today.getDate());
+            document.getElementById("dateFilter").value = today.getFullYear() +'-'+('0'+(today.getMonth()+1))+'-'+ ('0'+today.getDate());
+            }
+        }
+    }).then((status) => {
+        if(status.isConfirmed){
+            var encabezado = document.getElementById("encabezadoReporte").value;
+            var fileName = document.getElementById("nameFile").value;
+            var description = document.getElementById("descripcionReport").value;
+            var date = document.getElementById("dateCreate").value;
+            var dateFilter = document.getElementById("dateFilter").value;
+            var empleado = document.getElementById('empleado').value;
+            console.log(empleado);
+            if(dateFilter === undefined || dateFilter === null || dateFilter === ""){
+                dateFilter = date;
+            }
+            if(fileName != "" && description != "" && encabezado != ""){
+                $.post('.././controllers/CtrlConsultaReporteria.php', {
+                    generateReport: pet,
+                    fileName: fileName,
+                    header: encabezado,
+                    description: description,
+                    date : date,
+                    dateFilter: dateFilter,
+                    empleado: empleado
+                }).done((response) => {
+                    Toast.fire({
+                        icon: 'success',
+                        html: `
+                                <p>Reporte Generado Exitosamente</p>
+                                <a href="${response}" target="_blank">Abrir Reporte</a>
+                            `,
+                    });
+                }).fail((err) => {
+                    Toast.fire({
+                        icon: 'error',
+                        text: 'Error al generar reporte'
+                    });
+                });
+    
+            }
+        }        
+    });
 
-function dibujarGrafico() {
-
-    var data = google.visualization.arrayToDataTable([
-        ['Inventario', 'Productos'],
-        ['resma papel', 11],
-        ['Marcadores', 2],
-        ['Grapadora', 5]
-
-    ]);
-
-    var options = {
-        title: 'Stock de Productos',
-        pieHole: 0.4,
-        width: 650,
-        height: 500
-    };
-
-    var chart = new google.visualization.PieChart(document.getElementById('grafico'));
-    chart.draw(data, options);
 }
-<script src=".././js/bootstrap.bundle.min.js"></script>
--- views/sgRegUsuarios.php
+++ views/sgRegUsuarios.php
@@ -1,107 +1,151 @@
 <?php
 session_start();
-if($_SESSION['Oficina']['id']){
-  
+if ($_SESSION['Oficina']['id']) {
+    include($_SERVER['DOCUMENT_ROOT'] . '/Gestion-Inventario-Oficina/controllers/ctrlUsuarios.php');
+    $controller = new CtrlUsuarios();
+    $empleados = $controller->getEmpleadoSinUsuario();
+    $empleados = $empleados[2];
+    $usuarios = $controller->getUsuarios();
+    $usuarios = $usuarios[2];
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
+    <head>
 
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Usuarios</title>
-    <link rel="stylesheet" href=".././css/bootstrap.min.css">
-    <link rel="stylesheet" href=".././css/index.css">
-    <link rel="stylesheet" href=".././css/globalStyle.css">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Usuarios</title>
+        <link rel="stylesheet" href=".././css/bootstrap.min.css">
+        <link rel="stylesheet" href=".././css/index.css">
+        <link rel="stylesheet" href=".././css/globalStyle.css">
 
-    <link rel="shortcut icon" href=".././img/UTH-Black-favicon.png" type="image/x-icon">
-    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+        <link rel="shortcut icon" href=".././img/UTH-Black-favicon.png" type="image/x-icon">
+        <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
 
-</head>
+    </head>
 
-<body>
-    <?php include('.././components/nav-bar.php'); ?>
+    <body>
+        <?php include('.././components/nav-bar.php'); ?>
 
-    <section class="mt-container container-fluid">
-        <h2 class="text-center">Formulario de usuarios</h2>
+        <section class="mt-container container-fluid">
+            <h2 class="text-center">Formulario de usuarios</h2>
 
-        <div class="container-fluid row gx-5 px-4">
-            <!-- Inicio registro de empleados -->
-            <section class="col col-6 col-md-4">
-                <form>
-                    <h2 class="text-center">Formulario de Usuarios</h2>
-                   
+            <div class="container-fluid row gx-5 px-4">
+                <!-- Inicio registro de empleados -->
+                <section class="col col-6 col-md-4">
+                    <form>
+                        <h2 class="text-center">Formulario de Usuarios</h2>
+                        <label for="">Seleccione el Empleado: </label>
+                        <select name="idUsuario" id="" class="form-control">
+                            <option value="0">Seleccione</option>
+                            <?php for ($i = 0; $i < count($empleados); $i++) {
+                            ?>
+                                <option value="<?= $empleados[$i]['ID'] ?>"><?= $empleados[$i]['Empleado'] . ' - ' . $empleados[$i]['Identidad'] ?></option>
+                            <?php } ?>
+                        </select>
 
-                    <label for="inpuser" class="form-label">Ingrese un usuario: </label>
-                    <input type="text" class="form-control" id="inpuser" name="inpuser">
+                        <label for="inpuser" class="form-label">Ingrese un Alias: </label>
+                        <input type="text" class="form-control" id="inpuser" name="inpuser">
 
-                    <label for="inpPass" class="form-label">Ingrese la contraseña: </label>
-                    <input type="password" class="form-control" id="inpPass" name="inpPass">
+                        <label for="inpPass" class="form-label">Ingrese la contraseña: </label>
+                        <input type="password" class="form-control" id="inpPass" name="inpPass">
 
-                    <section class="row">
-                        <section class="col col-11">
-                            <label for="inpPassC" class="form-label">Confirmar contraseña: </label>
-                            <input type="password" class="form-control" id="inpPassC" name="inpPassC">
+                        <section class="row">
+                            <section class="col col-11">
+                                <label for="inpPassC" class="form-label">Confirmar contraseña: </label>
+                                <input type="password" class="form-control" id="inpPassC" name="inpPassC">
+                            </section>
+                            <section class="col col-1">
+                                <button type="button" onclick="ver();" style="margin-top:31px; margin-left: -25px;" class="btn">👁‍🗨</button>
+                            </section>
                         </section>
-                        <section class="col col-1">
-                            <button type="button" onclick="ver();" style="margin-top:31px; margin-left: -25px;" class="btn">👁‍🗨</button>
-                        </section>
+
+                        <label for="inpuser" class="form-label">Ingrese el nivel de Permiso para Datos Maestros: </label>
+                        <input type="number" class="form-control" id="pDatosM" name="" max="4">
+
+                        <label for="inpuser" class="form-label">Ingrese el nivel de Permiso para Transacciones: </label>
+                        <input type="number" class="form-control" id="pTransacciones" name="" max="4">
+
+                        <label for="inpuser" class="form-label">Ingrese el nivel de Permiso para Consultas y Reporteria: </label>
+                        <input type="number" class="form-control" id="pConsultaRep" name="" max="1">
+
+                        <label for="inpuser" class="form-label">Ingrese el nivel de Permiso para Seguridad: </label>
+                        <input type="number" class="form-control" id="pSeguridad" name="" max="4">
+                    </form>
+                    <br>
+                    <section class="btn-group mt-4" id="botnones" style="display: flex; justify-content: center;">
+                        <button type="button" class="btn btn-outline-primary" id="btnAgregar">Agregar</button>
+                        <button type="button" class="btn btn-outline-primary" id="btnCancelar">Cancelar</button>
                     </section>
-                </form>
-                <br>
-                <section class="btn-group mt-4" id="botnones" style="display: flex; justify-content: center;">
-                    <button type="button" class="btn btn-outline-primary" id="btnAgregar">Agregar</button>
-                    <button type="button" class="btn btn-outline-primary" id="btnCancelar">Cancelar</button>
                 </section>
-            </section>
-            <!-- Fin registro de empleados -->
+                <!-- Fin registro de empleados -->
 
-            <!-- Inicio de la tabla -->
-            <section class="col">
-                <table class="table table-hover" id="tabla">
-                    <thead>
-                        <tr>
-                            <th>Identidad</th>
-                            <th>Nombre</th>
-                            <th>Usuario</th>
-                            <th>Departamento</th>
-                        </tr>
-                    </thead>
-                    <tbody id="contentTable">
-                        
-                           
-                    </tbody>
-                </table>
-            </section>
-            <!-- Fin de la tabla -->
-        </div>
-    </section>
+                <!-- Inicio de la tabla -->
+                <section class="col">
+                    <table class="table table-hover" id="tabla">
+                        <thead>
+                            <tr>
+                                <th rowspan="2">ID</th>
+                                <th rowspan="2">Alias</th>
+                                <th rowspan="2">Empleado</th>
+                                <th colspan="4">Niveles de Permiso</th>
+                            </tr>
+                            <tr>
+                                <th>Datos Maestros</th>
+                                <th>Transacciones</th>
+                                <th>Consultas y Reporteria</th>
+                                <th>Seguridad</th>
+                            </tr>
+                            <tr>
+                                <th rowspan="2">Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody id="contentTable">
+                            <?php for($x=0; $x<count($usuarios); $x++){?>
+                                <tr>
+                                    <td><?= $usuarios[$x]['ID_EMPLEADO']?></td>
+                                    <td><?= $usuarios[$x]['alias']?></td>
+                                    <td><?= $usuarios[$x]['Empleado']?></td>
+                                    <td><?= $usuarios[$x]['dMaestros']?></td>
+                                    <td><?= $usuarios[$x]['transacciones']?></td>
+                                    <td><?= $usuarios[$x]['consultasReporteria']?></td>
+                                    <td><?= $usuarios[$x]['seguridad']?></td>
+                                </tr>
+                            <?php }?>
+                        </tbody>
+                    </table>
 
-    <script src=".././js/bootstrap.bundle.min.js"></script>
+                </section>
+            </div>
+        </section>
 
-    <script>
-        function ver() {
+        <script src=".././js/bootstrap.bundle.min.js"></script>
 
-            var campoContra = document.getElementById("inpPass");
-            var campoConfirm = document.getElementById("inpPassC");
+        <script>
+            function ver() {
 
-            if (campoContra.type === "password") {
-                campoContra.type = "text";
-                campoConfirm.type = "text";
-            } else {
-                campoContra.type = "password";
-                campoConfirm.type = "password";
+                var campoContra = document.getElementById("inpPass");
+                var campoConfirm = document.getElementById("inpPassC");
+
+                if (campoContra.type === "password") {
+                    campoContra.type = "text";
+                    campoConfirm.type = "text";
+                } else {
+                    campoContra.type = "password";
+                    campoConfirm.type = "password";
+
+                }
+
 
             }
+        </script>
+    </body>
 
-
-        }
-    </script>
-</body>
-
-</html>
+    </html>
 <?php
-}else{header('location: ../index.php');}
+} else {
+    header('location: ../index.php');
+}
 ?>
