8314f11479f01470914389dffb7ee4861deb7794
DempseyPrimi <72927786+DempseyPrimi@users.noreply.github.com>
2024-04-07 22:25:59 -0600

    CRUD View Usuarios

--- views/sgRegUsuarios.php
+++ views/sgRegUsuarios.php
@@ -20,6 +20,7 @@ if ($_SESSION['Oficina']['id']) {
         <link rel="stylesheet" href=".././css/bootstrap.min.css">
         <link rel="stylesheet" href=".././css/index.css">
         <link rel="stylesheet" href=".././css/globalStyle.css">
+        <link rel="stylesheet" href=".././css/nerdfont.css">
 
         <link rel="shortcut icon" href=".././img/UTH-Black-favicon.png" type="image/x-icon">
         <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
@@ -35,10 +36,10 @@ if ($_SESSION['Oficina']['id']) {
             <div class="container-fluid row gx-5 px-4">
                 <!-- Inicio registro de empleados -->
                 <section class="col col-6 col-md-4">
-                    <form>
+                    <form id="form-register-user">
                         <h2 class="text-center">Formulario de Usuarios</h2>
                         <label for="">Seleccione el Empleado: </label>
-                        <select name="idUsuario" id="" class="form-control">
+                        <select name="idUsuario" id="idUsuario" class="form-control" required>
                             <option value="0">Seleccione</option>
                             <?php for ($i = 0; $i < count($empleados); $i++) {
                             ?>
@@ -47,15 +48,15 @@ if ($_SESSION['Oficina']['id']) {
                         </select>
 
                         <label for="inpuser" class="form-label">Ingrese un Alias: </label>
-                        <input type="text" class="form-control" id="inpuser" name="inpuser">
+                        <input type="text" class="form-control" id="alias" name="alias" required>
 
                         <label for="inpPass" class="form-label">Ingrese la contraseña: </label>
-                        <input type="password" class="form-control" id="inpPass" name="inpPass">
+                        <input type="password" class="form-control" id="inpPass" name="inpPass" required>
 
                         <section class="row">
                             <section class="col col-11">
                                 <label for="inpPassC" class="form-label">Confirmar contraseña: </label>
-                                <input type="password" class="form-control" id="inpPassC" name="inpPassC">
+                                <input type="password" class="form-control" id="inpPassC" name="inpPassC" required>
                             </section>
                             <section class="col col-1">
                                 <button type="button" onclick="ver();" style="margin-top:31px; margin-left: -25px;" class="btn">👁‍🗨</button>
@@ -63,22 +64,22 @@ if ($_SESSION['Oficina']['id']) {
                         </section>
 
                         <label for="inpuser" class="form-label">Ingrese el nivel de Permiso para Datos Maestros: </label>
-                        <input type="number" class="form-control" id="pDatosM" name="" max="4">
+                        <input type="number" class="form-control" id="pDatosM" name="" min="0" max="4" value="0">
 
                         <label for="inpuser" class="form-label">Ingrese el nivel de Permiso para Transacciones: </label>
-                        <input type="number" class="form-control" id="pTransacciones" name="" max="4">
+                        <input type="number" class="form-control" id="pTransacciones" name="" min="0" max="4" value="0">
 
                         <label for="inpuser" class="form-label">Ingrese el nivel de Permiso para Consultas y Reporteria: </label>
-                        <input type="number" class="form-control" id="pConsultaRep" name="" max="1">
+                        <input type="number" class="form-control" id="pConsultaRep" name="" min="0" max="1" value="0">
 
                         <label for="inpuser" class="form-label">Ingrese el nivel de Permiso para Seguridad: </label>
-                        <input type="number" class="form-control" id="pSeguridad" name="" max="4">
+                        <input type="number" class="form-control" id="pSeguridad" name="" min="0" max="4" value="0">
+
+                        <section class="btn-group mt-4" id="botnones" style="display: flex; justify-content: center;">
+                            <button type="submit" class="btn btn-outline-primary" id="btnAgregar">Agregar</button>
+                        </section>
                     </form>
                     <br>
-                    <section class="btn-group mt-4" id="botnones" style="display: flex; justify-content: center;">
-                        <button type="button" class="btn btn-outline-primary" id="btnAgregar">Agregar</button>
-                        <button type="button" class="btn btn-outline-primary" id="btnCancelar">Cancelar</button>
-                    </section>
                 </section>
                 <!-- Fin registro de empleados -->
 
@@ -91,6 +92,7 @@ if ($_SESSION['Oficina']['id']) {
                                 <th rowspan="2">Alias</th>
                                 <th rowspan="2">Empleado</th>
                                 <th colspan="4">Niveles de Permiso</th>
+                                <th rowspan="2">Acciones</th>
                             </tr>
                             <tr>
                                 <th>Datos Maestros</th>
@@ -98,22 +100,27 @@ if ($_SESSION['Oficina']['id']) {
                                 <th>Consultas y Reporteria</th>
                                 <th>Seguridad</th>
                             </tr>
-                            <tr>
-                                <th rowspan="2">Acciones</th>
-                            </tr>
                         </thead>
                         <tbody id="contentTable">
-                            <?php for($x=0; $x<count($usuarios); $x++){?>
+                            <?php for ($x = 0; $x < count($usuarios); $x++) { ?>
                                 <tr>
-                                    <td><?= $usuarios[$x]['ID_EMPLEADO']?></td>
-                                    <td><?= $usuarios[$x]['alias']?></td>
-                                    <td><?= $usuarios[$x]['Empleado']?></td>
-                                    <td><?= $usuarios[$x]['dMaestros']?></td>
-                                    <td><?= $usuarios[$x]['transacciones']?></td>
-                                    <td><?= $usuarios[$x]['consultasReporteria']?></td>
-                                    <td><?= $usuarios[$x]['seguridad']?></td>
+                                    <td><?= $usuarios[$x]['ID_EMPLEADO'] ?></td>
+                                    <td><?= $usuarios[$x]['alias'] ?></td>
+                                    <td><?= $usuarios[$x]['Empleado'] ?></td>
+                                    <td><?= $usuarios[$x]['dMaestros'] ?></td>
+                                    <td><?= $usuarios[$x]['transacciones'] ?></td>
+                                    <td><?= $usuarios[$x]['consultasReporteria'] ?></td>
+                                    <td><?= $usuarios[$x]['seguridad'] ?></td>
+                                    <td>
+                                        <button class="btn btn-warning" onclick="editUsuario(<?= $usuarios[$x]['ID_EMPLEADO'] ?>)">
+                                            <i class="nf nf-md-pencil_outline"></i>
+                                        </button>
+                                        <button class="btn btn-danger" onclick="deleteUsuario(<?= $usuarios[$x]['ID_EMPLEADO'] ?>)">
+                                            <i class="nf nf-fa-trash_o"></i>
+                                        </button>
+                                    </td>
                                 </tr>
-                            <?php }?>
+                            <?php } ?>
                         </tbody>
                     </table>
 
@@ -122,25 +129,9 @@ if ($_SESSION['Oficina']['id']) {
         </section>
 
         <script src=".././js/bootstrap.bundle.min.js"></script>
-
-        <script>
-            function ver() {
-
-                var campoContra = document.getElementById("inpPass");
-                var campoConfirm = document.getElementById("inpPassC");
-
-                if (campoContra.type === "password") {
-                    campoContra.type = "text";
-                    campoConfirm.type = "text";
-                } else {
-                    campoContra.type = "password";
-                    campoConfirm.type = "password";
-
-                }
-
-
-            }
-        </script>
+        <script src=".././js/jquery-3.7.1.min.js"></script>
+        <script src=".././js/swal.min.js"></script>
+        <script src=".././js/segUsuarios.js"></script>
     </body>
 
     </html>
